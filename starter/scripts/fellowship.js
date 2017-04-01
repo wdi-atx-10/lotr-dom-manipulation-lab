5b2e6d319237ed49107fe66205b375afc1d2b4fa
Juliana Michelsen <julimichelsen@gmail.com>
2017-04-01 12:52:37 -0500

    updated hw files

--- starter/scripts/fellowship.js
+++ starter/scripts/fellowship.js
@@ -1,132 +1,168 @@
-console.log("Linked.");
+$(document).ready(function() {
+  console.log("Linked.");
 
-// Dramatis Personae
-var hobbits = [
-  'Frodo Baggins',
-  'Samwise \'Sam\' Gamgee',
-  'Meriadoc \'Merry\' Brandybuck',
-  'Peregrin \'Pippin\' Took'
-];
+  // Dramatis Personae
+  var hobbits = [
+    'Frodo Baggins',
+    'Samwise \'Sam\' Gamgee',
+    'Meriadoc \'Merry\' Brandybuck',
+    'Peregrin \'Pippin\' Took'
+  ];
 
-var buddies = [
-  'Gandalf the Grey',
-  'Legolas',
-  'Gimli',
-  'Strider',
-  'Boromir'
-];
+  var buddies = [
+    'Gandalf the Grey',
+    'Legolas',
+    'Gimli',
+    'Strider',
+    'Boromir'
+  ];
 
-var lands = ['The Shire', 'Rivendell', 'Mordor'];
-var body = document.querySelector('body');
+  var lands = ['The Shire', 'Rivendell', 'Mordor'];
+  var body = document.querySelector('body');
 
 
 // Part 1
+  // squad version
+  //$('body').append('<section id="middle-earth"></section>');
+  // $('#middle-earth').append('<article><h1>'+lands[i]+'</h1></article>');    //inside each article tag include an h1 w/ name of the lan
+  function makeMiddleEarth() {
+    $section = $('<section>').attr('id', 'middle-earth');
+
+      for (i=0; i<lands.length; i++) {
+        $article = $('<article>').html('<h1>' + lands[i] + '</h1>');
+        $section.append($article);
+        console.log('part 1');
+      }
+      $('body').append($section);
+  }
+
+  makeMiddleEarth();
 
 
-function makeMiddleEarth() {
-  // create a section tag with an id of middle-earth
-  // add each land as an article tag
-  // inside each article tag include an h1 with the name of the land
-  // append middle-earth to your document body
-}
-
-makeMiddleEarth();
-
-
-// Part 2
-
-function makeHobbits() {
+  // Part 2
   // display an unordered list of hobbits in the shire (which is the second article tag on the page)
   // give each hobbit a class of hobbit
-}
 
+  function makeHobbits() {
+    for (i=0; i<hobbits.length; i++) {
+      $('article:nth-child(1)').append('<ul><li class=hobbit>' + hobbits[i] + '</li></ul>');
+      console.log('part 2');
+    }
+  }
+  makeHobbits();
+  //CALL THE FUNCTION BACK!!!
 
-// Part 3
-
-function keepItSecretKeepItSafe() {
+  // Part 3
   // create a div with an id of 'the-ring'
   // give the div a class of 'magic-imbued-jewelry'
   // add an event listener so that when a user clicks on the ring, the nazgulScreech function (provided) is invoked
-  // add the ring as a child of Frodo
-}
+    // add the ring as a child of Frodo
+  function keepItSecretKeepItSafe() {
+    $div = $('div').attr( {
+      id: 'the-ring',
+      class: 'magic-imbued-jewelry'
+      });
+      $('#the-ring').click(nazgulScreech());
+    //  $('article ul li[0]').html() {
+      //  $(this).append('the-ring');
+        //}
+      console.log('part 3');
+  }
+
+  keepItSecretKeepItSafe();
+
+  // Part 4
 
 
-// Part 4
+  function makeBuddies() {
+    // create an aside tag
+    // attach an unordered list of the 'buddies' in the aside
+    // insert your aside as a child element of rivendell}
+    $aside = $('<aside>').attr('id', 'aside');
+      for (i=0; i<buddies.length; i++) {
+        $article2 = $('<ul><li>' + buddies[i] + '</li></ul>');
+        $aside.append($article2);
+      }
+        $('article:nth-child(2)').append($aside);
+        console.log('Part 4');
+  }
+
+  makeBuddies();
+
+  // Part 5
 
 
-function makeBuddies() {
-  // create an aside tag
-  // attach an unordered list of the 'buddies' in the aside
-  // insert your aside as a child element of rivendell
-}
+  function beautifulStranger() {
+    // change the 'Strider' textnode to 'Aragorn'
+  }
 
 
-// Part 5
+  // Part 6
+
+  function leaveTheShire() {
+    // assemble the hobbits and move them to Rivendell
+    $('.hobbit').append('article:nth-child(2)');
 
 
-function beautifulStranger() {
-  // change the 'Strider' textnode to 'Aragorn'
-}
+  }
 
 
-// Part 6
-
-function leaveTheShire() {
-  // assemble the hobbits and move them to Rivendell
-}
+  // Part 7
 
 
-// Part 7
+  function forgeTheFellowShip() {
+    // create a new div called 'the-fellowship' within rivendell
+    $theFellowship= $('<div>').attr('id', 'the-fellowship');
+      for(i=0, i<hobbits.length; i++)
+      $('article:nth-child(2)').append($theFellowship)
+    // add each hobbit and buddy one at a time to 'the-fellowship'
+    // after each character is added make an alert that they have joined your party
+  }
 
 
-function forgeTheFellowShip() {
-  // create a new div called 'the-fellowship' within rivendell
-  // add each hobbit and buddy one at a time to 'the-fellowship'
-  // after each character is added make an alert that they have joined your party
-}
+  // Part 8
 
 
-// Part 8
+  function theBalrog() {
+    // change the 'Gandalf' textNode to 'Gandalf the White'
+    // apply style to the element
+    // make the background 'white', add a grey border
+  }
 
 
-function theBalrog() {
-  // change the 'Gandalf' textNode to 'Gandalf the White'
-  // apply style to the element
-  // make the background 'white', add a grey border
-}
+  // Part 9
+
+  function hornOfGondor() {
+    // pop up an alert that the horn of gondor has been blown
+    alert()
+    // Boromir's been killed by the Uruk-hai!
+    // put a linethrough on boromir's name
+    // Remove Boromir from the Fellowship
+  }
 
 
-// Part 9
+  // Part 10
 
-function hornOfGondor() {
-  // pop up an alert that the horn of gondor has been blown
-  // Boromir's been killed by the Uruk-hai!
-  // put a linethrough on boromir's name
-  // Remove Boromir from the Fellowship
-}
+  function itsDangerousToGoAlone(){
+    // take Frodo and Sam out of the fellowship and move them to Mordor
+    // add a div with an id of 'mount-doom' to Mordor
+  }
 
 
-// Part 10
+  // Part 11
 
-function itsDangerousToGoAlone(){
-  // take Frodo and Sam out of the fellowship and move them to Mordor
-  // add a div with an id of 'mount-doom' to Mordor
-}
-
-
-// Part 11
-
-function weWantsIt() {
-  // Create a div with an id of 'gollum' and add it to Mordor
-  // Remove the ring from Frodo and give it to Gollum
-  // Move Gollum into Mount Doom
-}
+  function weWantsIt() {
+    // Create a div with an id of 'gollum' and add it to Mordor
+    // Remove the ring from Frodo and give it to Gollum
+    // Move Gollum into Mount Doom
+  }
 
 
-// Part 12
+  // Part 12
 
-function thereAndBackAgain() {
-  // remove Gollum and the Ring from the document
-  // remove all the baddies from the document
-  // Move all the hobbits back to the shire
-}
+  function thereAndBackAgain() {
+    // remove Gollum and the Ring from the document
+    // remove all the baddies from the document
+    // Move all the hobbits back to the shire
+  }
+});
